f5475f1669a02888dd1c3368a7b0f5ea7fee4b3a
Deni Barasena <Deni Barasena@DENI-ASUS.gateway>
2017-05-18 12:17:45 +1000

    Initial stuffs

---
+++ src/java/Model/Employee.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author Deni Barasena
+ */
+public class Employee {
+    private int employeeID;
+    private String name;
+    private String email;
+    private String contacts;
+}
---
+++ src/java/Model/User.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author Deni Barasena
+ */
+public class User {
+    private int userID;
+    private String fullname;
+    private String email;
+    private String password;
+
+    public User(int userID, String fullname, String email, String password) {
+        this.userID = userID;
+        this.fullname = fullname;
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+    
+}
